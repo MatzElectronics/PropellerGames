a96e93fb0aa8b5de05eb5ef76a6d56c48256812d
Matthew Matz <19737272+MatzElectronics@users.noreply.github.com>
2018-11-05 13:31:33 -0800

    Update snake.c

--- Snake/snake.c
+++ Snake/snake.c
@@ -1,13 +1,14 @@
 // ------ Libraries and Definitions ------
 #include "simpletools.h"
 #include "oledc.h"
+#include "colormath.h"
+
 #define MY_SCREEN_WIDTH 96
 #define MY_SCREEN_HEIGHT 64
 #define MY_X_SIZE 91
 #define MY_Y_SIZE 59
 #define MY_BORDER 2
 #define MY_SEGMENT_SIZE 3
-#include "colormath.h"
 
 // ------ Global Variables and Objects ------
 int snakeX[250];
@@ -50,21 +51,17 @@ int main() {
       if (breakFlag) {
         break;
       }
-
       if (collisionFlag) {
         collisionFlag = 0;
         break;
       }
-
       pause(200);
     }
     if (breakFlag) {
       breakFlag = 0;
       break;
     }
-
   }
-
 }
 
 // ------ Functions ------
@@ -73,20 +70,15 @@ void checkSelfCollision() {
   while (1) {
     if (item > snakeSize) {
       item = 1;
-    }
-    else
-    {
+    } else {
       item++;
     }
-
     if (snakeX[item] == snakeX[snakeHead] && snakeY[item] == snakeY[snakeHead]) {
       collisionFlag = 1;
     }
-
     if (item == snakeHead) {
       break;
     }
-
   }
 }
 
@@ -94,20 +86,16 @@ void checkFood() {
   if (foodX == (snakeX[snakeHead] + directionX) && foodY == (snakeY[snakeHead] + directionY)) {
     ateFood = 1;
   }
-
 }
 
 void checkWallCollision() {
   if (snakeX[snakeHead] < 0) {
     collisionFlag = 1;
-  }
-  else if (snakeY[snakeHead] < 0) {
+  } else if (snakeY[snakeHead] < 0) {
     collisionFlag = 1;
-  }
-  else if (snakeX[snakeHead] >= ((MY_SCREEN_WIDTH - MY_BORDER) / MY_SEGMENT_SIZE)) {
+  } else if (snakeX[snakeHead] >= ((MY_SCREEN_WIDTH - MY_BORDER) / MY_SEGMENT_SIZE)) {
     collisionFlag = 1;
-  }
-  else if (snakeY[snakeHead] >= ((MY_SCREEN_HEIGHT - MY_BORDER) / MY_SEGMENT_SIZE)) {
+  } else if (snakeY[snakeHead] >= ((MY_SCREEN_HEIGHT - MY_BORDER) / MY_SEGMENT_SIZE)) {
     collisionFlag = 1;
   }
 }
@@ -116,23 +104,19 @@ void checkButtons() {
   while (1) {
     if (!input(6) && !input(5)) {
       breakFlag = 1;
-    }
-    else if (!input(7)) {
+    } else if (!input(7)) {
       directionX = 1;
       directionY = 0;
       pause(200);
-    }
-    else if (!input(6)) {
+    } else if (!input(6)) {
       directionY = 1;
       directionX = 0;
       pause(200);
-    }
-    else if (!input(5)) {
+    } else if (!input(5)) {
       directionY = -1;
       directionX = 0;
       pause(200);
-    }
-    else if (!input(4)) {
+    } else if (!input(4)) {
       directionX = -1;
       directionY = 0;
       pause(200);
@@ -142,41 +126,34 @@ void checkButtons() {
 
 void moveSnake() {
   if (!ateFood) {
-    oledc_fillRect(snakeX[snakeTail] * MY_SEGMENT_SIZE + MY_BORDER, snakeY[snakeTail] * MY_SEGMENT_SIZE + MY_BORDER, MY_SEGMENT_SIZE, MY_SEGMENT_SIZE, oledc_color565(get8bitColor(0x000000, "RED"), get8bitColor(0x000000, "GREEN"), get8bitColor(0x000000, "BLUE")));
+    oledc_fillRect(snakeX[snakeTail] * MY_SEGMENT_SIZE + MY_BORDER, snakeY[snakeTail] * MY_SEGMENT_SIZE + MY_BORDER, MY_SEGMENT_SIZE, MY_SEGMENT_SIZE, 0);
     snakeTail++;
     if (snakeTail > snakeSize) {
       snakeTail = 1;
     }
-
-  }
-  else
-  {
+  } else {
     if ((snakeHead + 1) == snakeTail) {
       for (item = snakeSize; item > snakeTail; item--) {
-        snakeX[constrainInt(1 + item, 0, 249)] = snakeX[item];
-        snakeY[constrainInt(1 + item, 0, 249)] = snakeY[item];
-
+        snakeX[1 + item] = snakeX[item];
+        snakeY[1 + item] = snakeY[item];
       }
-      oledc_fillRect(snakeX[snakeTail] * MY_SEGMENT_SIZE + MY_BORDER, snakeY[snakeTail] * MY_SEGMENT_SIZE + MY_BORDER, MY_SEGMENT_SIZE, MY_SEGMENT_SIZE, oledc_color565(get8bitColor(0x000000, "RED"), get8bitColor(0x000000, "GREEN"), get8bitColor(0x000000, "BLUE")));
+      oledc_fillRect(snakeX[snakeTail] * MY_SEGMENT_SIZE + MY_BORDER, snakeY[snakeTail] * MY_SEGMENT_SIZE + MY_BORDER, MY_SEGMENT_SIZE, MY_SEGMENT_SIZE, 0);
       snakeTail++;
     }
-
     snakeSize++;
     ateFood = 0;
     placeFood();
   }
-
   headX = (snakeX[snakeHead] + directionX);
   headY = (snakeY[snakeHead] + directionY);
   snakeHead++;
   if (snakeHead > snakeSize) {
     snakeHead = 1;
   }
-
-  snakeX[constrainInt(snakeHead, 0, 249)] = headX;
-  snakeY[constrainInt(snakeHead, 0, 249)] = headY;
-  oledc_fillRect(snakeX[snakeHead] * MY_SEGMENT_SIZE + MY_BORDER, headY * MY_SEGMENT_SIZE + MY_BORDER, MY_SEGMENT_SIZE, MY_SEGMENT_SIZE, oledc_color565(get8bitColor(0x00ef00, "RED"), get8bitColor(0x00ef00, "GREEN"), get8bitColor(0x00ef00, "BLUE")));
-  oledc_fillRect(foodX * MY_SEGMENT_SIZE + MY_BORDER, foodY * MY_SEGMENT_SIZE + MY_BORDER, MY_SEGMENT_SIZE, MY_SEGMENT_SIZE, oledc_color565(get8bitColor(0x277dff, "RED"), get8bitColor(0x277dff, "GREEN"), get8bitColor(0x277dff, "BLUE")));
+  snakeX[snakeHead] = headX;
+  snakeY[snakeHead] = headY;
+  oledc_fillRect(snakeX[snakeHead] * MY_SEGMENT_SIZE + MY_BORDER, headY * MY_SEGMENT_SIZE + MY_BORDER, MY_SEGMENT_SIZE, MY_SEGMENT_SIZE, oledc_color565(0, 0xFF, 0);
+  oledc_fillRect(foodX * MY_SEGMENT_SIZE + MY_BORDER, foodY * MY_SEGMENT_SIZE + MY_BORDER, MY_SEGMENT_SIZE, MY_SEGMENT_SIZE, oledc_color565(0x27, 0x7D, 0xFF));
 }
 
 void resetGame() {
@@ -194,14 +171,13 @@ void resetGame() {
   directionY = 0;
   collisionFlag = 0;
   oledc_clear(0, 0, oledc_getWidth(), oledc_getHeight());
-  oledc_fillRect(0, 0, MY_SCREEN_WIDTH, MY_BORDER, oledc_color565(get8bitColor(0xef0000, "RED"), get8bitColor(0xef0000, "GREEN"), get8bitColor(0xef0000, "BLUE")));
-  oledc_fillRect(0, MY_SCREEN_HEIGHT - MY_BORDER, MY_SCREEN_WIDTH, MY_BORDER, oledc_color565(get8bitColor(0xef0000, "RED"), get8bitColor(0xef0000, "GREEN"), get8bitColor(0xef0000, "BLUE")));
-  oledc_fillRect(0, 0, MY_BORDER, MY_SCREEN_HEIGHT, oledc_color565(get8bitColor(0xef0000, "RED"), get8bitColor(0xef0000, "GREEN"), get8bitColor(0xef0000, "BLUE")));
-  oledc_fillRect(MY_SCREEN_WIDTH - MY_BORDER, 0, MY_BORDER, MY_SCREEN_HEIGHT, oledc_color565(get8bitColor(0xef0000, "RED"), get8bitColor(0xef0000, "GREEN"), get8bitColor(0xef0000, "BLUE")));
+  oledc_fillRect(0, 0, MY_SCREEN_WIDTH, MY_BORDER, oledc_color565(0xFF, 0, 0);
+  oledc_fillRect(0, MY_SCREEN_HEIGHT - MY_BORDER, MY_SCREEN_WIDTH, MY_BORDER, oledc_color565(0xFF, 0, 0));
+  oledc_fillRect(0, 0, MY_BORDER, MY_SCREEN_HEIGHT, oledc_color565(get8bitColor(0xFF, 0, 0));
+  oledc_fillRect(MY_SCREEN_WIDTH - MY_BORDER, 0, MY_BORDER, MY_SCREEN_HEIGHT, oledc_color565(0xFF, 0, 0));
   placeFood();
   for (item = snakeTail; item <= snakeHead; item++) {
-    oledc_fillRect(snakeX[item] * MY_SEGMENT_SIZE + MY_BORDER, snakeY[item] * MY_SEGMENT_SIZE + MY_BORDER, MY_SEGMENT_SIZE, MY_SEGMENT_SIZE, oledc_color565(get8bitColor(0x00ef00, "RED"), get8bitColor(0x00ef00, "GREEN"), get8bitColor(0x00ef00, "BLUE")));
-
+    oledc_fillRect(snakeX[item] * MY_SEGMENT_SIZE + MY_BORDER, snakeY[item] * MY_SEGMENT_SIZE + MY_BORDER, MY_SEGMENT_SIZE, MY_SEGMENT_SIZE, oledc_color565(0, 0xFF, 0));
   }
 }
 
@@ -214,24 +190,18 @@ void placeFood() {
     while (1) {
       if (item > snakeSize) {
         item = 1;
-      }
-      else
-      {
+      } else {
         item++;
       }
-
       if (foodX == snakeX[item] && foodY == snakeY[item]) {
         foodCheck = 0;
       }
-
       if (item == snakeHead) {
         break;
       }
-
     }
     if (foodCheck) {
       break;
     }
-
   }
 }
