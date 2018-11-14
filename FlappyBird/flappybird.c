4ca754ccfc1e5853e312485060fa78627f47301c
Matthew Matz <19737272+MatzElectronics@users.noreply.github.com>
2018-11-14 01:47:46 -0800

    Update flappybird.c

--- FlappyBird/flappybird.c
+++ FlappyBird/flappybird.c
@@ -1,103 +1,87 @@
-#include "simpletools.h"                      // Include simple tools
+// ------ Libraries and Definitions ------
+#include "simpletools.h"
 #include "oledc.h"
+#include "colormath.h"
 
+// ------ Global Variables and Objects ------
+int gameSpeed;
+int pipeGap;
+int pipeHeight;
+int birdPower;
+int loc;
+int acceleration;
+int pipeWidth;
+int birdPosition;
+int btnSet;
+int score;
+int birdSpeed;
 
-int main() {                                  // Main function
+// ------ Function Declarations ------
+void setup_game();
 
-  oledc_init(7, 6, 9, 8, 5, 1); // pins: CS, D/C, DIN, CLK, RES   rotation: 0 (pins upward)
-
-  srand(CNT + INA);
-
-  int speed = 75;
-  int bird = 15;
-  int btnSet = 0;
-  int theScore = 0;
-  int collision = 0;
-
-  oledc_fillRect(0,0,oledc_getWidth(),17,NAVYBLUE);
-
-  while(1) {
-    oledc_setCursor(0,0,0);
-    oledc_setTextColor(WHITE, WHITE);
-    oledc_setTextSize(2);  
-    
-    int pipeHeight = rand() & 30 - 15;
-    for(int d = 0; d < oledc_getWidth()+7; d++) {
-      //draw pipes
-      int pipeWidth = 8;
-      if(d > oledc_getWidth()) {
-        pipeWidth = 8 - d + oledc_getWidth();
-      }
-      oledc_fillRect(oledc_getWidth()-d,18,pipeWidth,28 + pipeHeight,oledc_color565(0,255,0));
-      oledc_fillRect(oledc_getWidth()-d,oledc_getHeight()-(28 - pipeHeight),pipeWidth,28 - pipeHeight,oledc_color565(0,255,0));
-      
-
-      //check for a button press
-      if(input(2) == 0 && btnSet == 0) {
+// ------ Main Program ------
+int main() {
+  oledc_init(22, 21, 20, 19, 18, 2);
+  setup_game();
+  while (1) {
+    pipeHeight = (random(0, ((oledc_getHeight()) - pipeGap)));
+    for (loc = 0; loc < (oledc_getWidth()) + 8; loc++) {
+      oledc_fillRect((oledc_getWidth()) - loc, 11, pipeWidth, pipeHeight, oledc_color565(0x33, 0xCC, 0));
+      oledc_fillRect((oledc_getWidth()) - loc, pipeHeight + pipeGap, pipeWidth, oledc_getHeight(), oledc_color565(0x33, 0xCC, 0));
+      if (!input(4) && !btnSet) {
         btnSet = 1;
-        speed = 32;
+        birdSpeed = birdPower;
       }
-      
-      //button debounce
-      if(input(2) == 1 && btnSet == 1) btnSet = 0;
-      
-      //set bird's position        
-      if(bird < 0) {
-        bird = 0;
-      }        
-      bird = bird + speed / 10;
-      speed = speed - 6;
-      if(bird > oledc_getHeight()-18) {
-        bird = oledc_getHeight()-18;
+
+      if (input(4) && btnSet) {
+        btnSet = 0;
       }
-      
-      //did they score?
-      if(d == oledc_getWidth() - 2) {
-        theScore++;
-        oledc_fillRect(0,0,oledc_getWidth(),17,NAVYBLUE);
-        oledc_print(" %d", theScore);
-      }        
-      
-      
-      //draw bird
-      oledc_fillRect(10,oledc_getHeight() - bird,5,5,YELLOWORANGE);
-      
-      //test for crash
-      if(d >= (oledc_getWidth() - 10) && d <= (oledc_getWidth() - 2)) {
-        if(bird < 28 + 5 - pipeHeight || bird > (oledc_getHeight() - 28 - 18 - pipeHeight)) {
-          collision = 1;
+
+      birdPosition = (birdPosition - birdSpeed / 10);
+      birdSpeed = (birdSpeed - acceleration);
+      birdPosition = (birdPosition > 11 ? birdPosition : 11);
+      birdPosition = ((birdPosition < (oledc_getHeight()) ? birdPosition : (oledc_getHeight())));
+      oledc_fillRect(10, birdPosition, 5, 5, oledc_color565(255, 255, 0));
+      if (loc < ((oledc_getWidth()) - 2) && loc > ((oledc_getWidth()) - 10)) {
+        if (birdPosition < (pipeHeight + 10) || birdPosition > (pipeHeight + pipeGap - 5)) {
+          oledc_setTextColor(oledc_color565(255,255,255), oledc_color565(0xCC, 0, 0));
+          oledc_setCursor(10, 20, 0);
+          oledc_drawText("Game Over");
+          while (input(4)) {}
+          pause(250);
+          oledc_clear(0, 0, oledc_getWidth(), oledc_getHeight());
+          setup_game();
           break;
         }
-      }                  
-      
-      //wait, then erase pipes and bird
-      pause(30);
-      oledc_fillRect(10,oledc_getHeight() - bird,5,5,BLACK);
-      oledc_fillRect(oledc_getWidth()-d,18,pipeWidth,28 + pipeHeight,BLACK);
-      oledc_fillRect(oledc_getWidth()-d,oledc_getHeight()-(28 - pipeHeight),pipeWidth,28 - pipeHeight,BLACK);
-    }    
-    
-    //was there a collision?
-    if(collision) {
-      //print "Game Over" on the screen
-      oledc_setCursor(0,2,2);
-      oledc_setTextColor(WHITE,DARKRED);
-      oledc_print("Game \nOver ");
-      
-      //wait for a button press
-      while(input(2));
-      pause(250);
-      
-      //reset all of the variables
-      speed = 75;
-      bird = 15;
-      btnSet = 0;
-      theScore = 0;
-      collision = 0;
-    
-      //clear the screen and replace the top dark blue box
-      oledc_clear(0,0,oledc_getWidth(),oledc_getHeight());
-      oledc_fillRect(0,0,oledc_getWidth(),17,NAVYBLUE);
-    }      
-  }    
+
+      }
+
+      if (loc == ((oledc_getWidth()) - 2)) {
+        score++;
+        oledc_setCursor(1, 1, 0);
+        oledc_print("%s%d%c", "Score = ", score, 32);
+      }
+
+      pause(gameSpeed);
+      oledc_fillRect(0, 11, oledc_getWidth(), (oledc_getHeight()) - 11, 0);
+
+    }
+  }
+
+}
+
+// ------ Functions ------
+void setup_game() {
+  gameSpeed = 25;
+  pipeGap = 17;
+  birdPower = 30;
+  acceleration = 4;
+  pipeWidth = 8;
+  birdPosition = 40;
+  btnSet = 0;
+  score = 0;
+  pipeGap = (pipeGap + 11);
+  birdSpeed = birdPower;
+  oledc_setTextColor(oledc_color565(255,255,255), oledc_color565(0,0,0x99));
+  oledc_fillRect(0, 0, oledc_getWidth(), 10, oledc_color565(0,0,0x99));
 }
